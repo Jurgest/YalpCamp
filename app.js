3e43b0cb998257e82f91a06d08da9cc5920aa80b
Jurgest <ballajurgest@gmail.com>
2021-10-03 20:39:21 +0200

    add detail page

--- app.js
+++ app.js
@@ -11,14 +11,16 @@ app.set('view engine', 'ejs');
 // SCHEMA setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campgrounds = mongoose.model('Campground', campgroundSchema);
 // Campgrounds.create(
 //     {
-//         name: 'Camp 2',
-//         image:'https://pixabay.com/get/g6d464dba6484798fc09a63efc50b752d2d7cbf1aed02a9a9b42a073331f7dd572b75dcc56e767e0a7ee8e8218249f752_340.jpg' 
+//         name: 'Camp 9',
+//         image:'https://www.photosforclass.com/download/pb_4817872',
+//         description:'a popular camp'
 //     }, (err, campground)=> {
 //         if(err) {
 //             console.log(err);
@@ -33,27 +35,28 @@ var Campgrounds = mongoose.model('Campground', campgroundSchema);
 app.get('/', (req, res) => {
     res.render('landing');
 });
-
+// INDEX ROUTE
 app.get('/campgrounds', (req, res)=> {
     // res.render('campgrounds', {campgrounds: campgrounds});
     Campgrounds.find({}, (err, allCampgrounds)=> {
         if(err) {
             console.log(err)
         } else{
-            res.render('campgrounds', {campgrounds:allCampgrounds});
+            res.render('index', {campgrounds:allCampgrounds});
         }
     });
 });
-
+// SHOW FORM for new cg
 app.get('/campgrounds/new', (req, res)=> {
     res.render('new.ejs');
 });
-
+// CREATE 
 app.post('/campgrounds', (req, res)=> {
     // get data from form and add to campground array
     var name = req.body.name ;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description:desc};
     Campgrounds.create(newCampground,(err, newlyCreated) =>{
         if(err) {
             console.log(err)
@@ -61,8 +64,20 @@ app.post('/campgrounds', (req, res)=> {
             res.redirect('/campgrounds');
         }
     });
-    
-    
+});
+// SHOW DETAILED PAGE
+app.get('/campgrounds/:id', (req, res) => {
+    // find the ID
+    Campgrounds.findById(req.params.id, (err,foundCampground) => {
+        if(err) {
+            console.log(err)
+        } else {
+                // render show template
+                 res.render('show', {campground: foundCampground});
+        }
+    })
+     
+
 });
 
 app.listen(3000, () => {
