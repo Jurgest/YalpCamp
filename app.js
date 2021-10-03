d61bc1859cf6eb95bcca1805b6827596fca9ce66
Jurgest <ballajurgest@gmail.com>
2021-10-03 13:36:47 +0200

    add styling

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ app.set('view engine', 'ejs');
 var campgrounds = [
     {name: 'First mountin', image:'https://pixabay.com/get/ge84e8df0a74e2375155fef13bd48877407d192ff9225a48e4b6788c2b10e1affc84abd8f082220b8e32a44240563354a_340.jpg'},
     {name: 'second mountin', image:'https://pixabay.com/get/g6d464dba6484798fc09a63efc50b752d2d7cbf1aed02a9a9b42a073331f7dd572b75dcc56e767e0a7ee8e8218249f752_340.jpg'},
+    {name: 'third mountin', image:'https://pixabay.com/get/g7949718ac7fec181a78bf9a2aed9ef8f372e217dd3025e5fab0adfd85fc441ebbaaab9d0945eb187b62261ea4ff2b770_340.jpg'},
+    {name: 'First mountin', image:'https://pixabay.com/get/ge84e8df0a74e2375155fef13bd48877407d192ff9225a48e4b6788c2b10e1affc84abd8f082220b8e32a44240563354a_340.jpg'},
+    {name: 'second mountin', image:'https://pixabay.com/get/g6d464dba6484798fc09a63efc50b752d2d7cbf1aed02a9a9b42a073331f7dd572b75dcc56e767e0a7ee8e8218249f752_340.jpg'},
+    {name: 'third mountin', image:'https://pixabay.com/get/g7949718ac7fec181a78bf9a2aed9ef8f372e217dd3025e5fab0adfd85fc441ebbaaab9d0945eb187b62261ea4ff2b770_340.jpg'},
+    {name: 'First mountin', image:'https://pixabay.com/get/ge84e8df0a74e2375155fef13bd48877407d192ff9225a48e4b6788c2b10e1affc84abd8f082220b8e32a44240563354a_340.jpg'},
+    {name: 'second mountin', image:'https://pixabay.com/get/g6d464dba6484798fc09a63efc50b752d2d7cbf1aed02a9a9b42a073331f7dd572b75dcc56e767e0a7ee8e8218249f752_340.jpg'},
     {name: 'third mountin', image:'https://pixabay.com/get/g7949718ac7fec181a78bf9a2aed9ef8f372e217dd3025e5fab0adfd85fc441ebbaaab9d0945eb187b62261ea4ff2b770_340.jpg'}
+
 ];
 
 app.get('/', (req, res) => {
